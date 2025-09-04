64c82aa8ea9bfdf583b6db4a07164a37583e6929
HenryBracke <jbracke26@nmhschool.org>
2025-09-04 09:36:23 -0400

    finished random_quote_generator

--- iteration_01/assignments/day_05/random_quote_generator.py
+++ iteration_01/assignments/day_05/random_quote_generator.py
@@ -5,21 +5,36 @@
 # Step 3: Define a function that returns a random quote when called
 # Step 4: Define a main function that runs the program. Make sure to call the function.
 
+from pathlib import Path
+import random
+
+BASE_DIR = Path(__file__).parent
+quotes_path = BASE_DIR / "quotes.txt"
+
+
 # Functions takes in filename parameter and returns list of strings with lines from file
 def load_quotes(filename):
-    return None
+    with open (filename, 'r') as file:
+        quotes = file.readlines()
+        quotes = [quote.strip("\n") for quote in quotes]
+    return quotes
 
 # Function takes in list of strings and randomly chooses one to return
 def get_random_quote(quotes):
-    return None
+    return random.choice(quotes)
 
 # Runs program. Main() is the only function called so that it calls the other functions appropriately and controls logic flow.
 def main():
-    return None 
+    quotes = load_quotes(quotes_path)
+    while True: 
+        user_input = input("Enter Quote to get a random quote, enter Exit to exit.")
+        if user_input == "Exit":
+            break
+        elif user_input == "Quote":
+                random_quote = get_random_quote(quotes)
+                print(random_quote)
+        else :
+            print("Invalid input. Please enter 'Quote' or 'Exit'.")
+    return None
 
 main()
-
-
-
-
-
